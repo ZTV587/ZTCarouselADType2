93ed9f3590929a7e272749b6816625c2eff7bb16
zzt <zzt@qq.com>
2016-09-03 09:31:32 +0800

    ZTCarouselADType2 , Carousel AD, AD

---
+++ ZTCarouselADType2/TitleViewControllers/SixthViewController.m
@@ -0,0 +1,21 @@
+//
+//  Created by ZZT on 15/9/15.
+//  Copyright © 2015年 ZZT. All rights reserved.
+//
+
+#import "SixthViewController.h"
+
+@interface SixthViewController ()
+
+@end
+
+@implementation SixthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+
+@end
---
+++ ZTCarouselADType2/TitleViewControllers/ThreeViewController.h
@@ -0,0 +1,10 @@
+//
+//  Created by ZZT on 15/9/15.
+//  Copyright © 2015年 ZZT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreeViewController : UIViewController
+
+@end
